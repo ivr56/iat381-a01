a66da3d587b539936c0d482d89cbb1184c2a3082
ivr56 <ivor.sagala@gmail.com>
2015-01-27 11:24:10 -0800

    New View Controller

---
+++ src/app/controllers/q1.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//Controller
+angular.module('iat381A01')
+  .controller('Q1Ctrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
