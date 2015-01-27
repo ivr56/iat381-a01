e43b5b0dbe7ff6b891f06e5fba6c919fec79e813
ivr56 <ivor.sagala@gmail.com>
2015-01-27 11:06:09 -0800

    Test Q Pages

--- src/app/index.js
+++ src/app/index.js
@@ -3,20 +3,34 @@
 angular.module('iat381A01', 
                ['ngAnimate', 'ngRoute','ngCookies', 'ngTouch', 'ngSanitize', 'ui.bootstrap', ])
 
-  .config(function ($routeProvider) {
+//Route Provider
+//When
+//Local URL
+//Controller Name
+
+.config(function ($routeProvider) {
     $routeProvider
+    //Landing Page
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+    //About Quiz Page
       .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+    //Questions Begin 
        .when('/q1', {
         templateUrl: 'views/q1.html',
         controller: 'Q1Ctrl'
       })
+    
+    //Questions End
+    
+    
+    
+    //Defult Fallback
       .otherwise({
         redirectTo: '/'
       });
