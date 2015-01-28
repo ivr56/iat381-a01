db8c50f4213faacd46888dad7c7c5cd7150f30b4
ivr56 <ivor.sagala@gmail.com>
2015-01-28 13:48:44 -0800

    Test

---
+++ src/app/logic/logic_temp.js
@@ -0,0 +1,18 @@
+var neg_response_true = 0;
+var neg_response_false = 0;
+var btn_pressed = 0;
+
+
+
+document.getElementById('btn_01').onclick = function() 
+{
+        
+document.getElementById('q1_head').innerHTML='---future---';
+    
+    btn_pressed = 2;
+    
+};
+
+
+
+
