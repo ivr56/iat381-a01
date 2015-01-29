85e9a5dfa468b8f801c53b87df7857a34a6822b4
ivr56 <ivor.sagala@gmail.com>
2015-01-28 19:24:08 -0800

    New Logic

---
+++ src/app/logic/quizcalculator.js
@@ -0,0 +1,13 @@
+angular.module('iat381A01')
+    .factory('quizcalc', function () {
+    
+        var propertyint = 0;
+    
+    return {
+        propertyint: { data: 0 }
+                        
+        };
+        
+});
+
+
