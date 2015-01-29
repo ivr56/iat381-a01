d9bd3819d2ceb702533d0eb7d32b0a86edf867d7
ivr56 <ivor.sagala@gmail.com>
2015-01-28 17:55:27 -0800

    Changes to Logic

--- src/app/logic/logic_temp.js
+++ src/app/logic/logic_temp.js
@@ -2,16 +2,23 @@ var neg_response_true = 0;
 var neg_response_false = 0;
 var btn_pressed = 0;
 
+var response_1 = 0;
 
-
-document.getElementById('btn_01').onclick = function() 
+if (response_1 > 0)
 {
+   document.getElementById('q2_head').innerHTML='---ballin---';
+     
+}
+
+//document.getElementById('btn_01').onclick //= function() 
+//{
         
-document.getElementById('q1_head').innerHTML='---future---';
+//document.getElementById('q1_head').innerHT//ML='---future---';
     
-    btn_pressed = 2;
     
-};
+//};
+//
+
 
 
 
