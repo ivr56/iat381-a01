3965b56cc403f60a48a5378df522acbbd15cbf29
ivr56 <ivor.sagala@gmail.com>
2015-01-31 19:52:51 -0800

    Changes to Image Service

---
+++ dist/controllers/results.js
@@ -0,0 +1,8 @@
+  
+    //------------------
+    //Result Controller
+   angular.module('iat381-a01')   
+    .controller('resultcontroller', function($scope) {
+      $scope.text = "hello";
+    })
+     
---
+++ src/app/controllers/imagecontroller.js
@@ -0,0 +1,18 @@
+
+  //------------------
+  //Image Controller Start
+  //Angualr Start
+
+angular.module('iat381-a01')
+.controller('imagecontroller', function($scope) 
+    {
+    
+    $scope.$on('$routeChangeSuccess', function()
+               {
+                  
+               });
+
+   
+})
+  //End Image Controller
+  //------------------
