a6e1bea304f06c9ea49e9cc70ce041a0b8d11319
ivr56 <ivor.sagala@gmail.com>
2015-01-31 17:37:34 -0800

    New Logic Form

--- src/app/controllers/log.js
+++
@@ -1,8 +0,0 @@
-
-angular.module('iat381A01', 
-               ['ngAnimate', 'ngRoute','ngCookies', 'ngTouch', 'ngSanitize', 'ui.bootstrap', ])
-.controller('logcontroller', function($scope, $rootScope) {
-      console.log("Check Log"); 
-      $rootScope.quizset = Math.floor((Math.random() * 10) + 1);;
-      console.log("Quiz #: " + $rootScope.quizset); 
-    });
