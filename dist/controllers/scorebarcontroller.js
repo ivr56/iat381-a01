02025575e40ede21f7c220c926d9f5dfcabadf1a
ivr56 <ivor.sagala@gmail.com>
2015-02-04 13:56:38 -0800

    New Changes

--- dist/controllers/scorebarcontroller.js
+++
@@ -1,9 +0,0 @@
-  angular.module('iat381-a01')   
-    .controller('scorebarcontroller', function($scope, $rootScope) {
-
-      $scope.scoring = $rootScope.score;
-      $scope.correct = $rootScope.corrected;
-    
-    })
-     
-     
